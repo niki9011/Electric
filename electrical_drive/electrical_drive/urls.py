0d2eb7869eaf17f5cc4cced3debbfe69fdab521d
Nikolay Videnov <104257986+niki9011@users.noreply.github.com>
2023-07-26 00:32:01 +0300

    Update README.md

--- electrical_drive/electrical_drive/urls.py
+++ electrical_drive/electrical_drive/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for electrical_drive project.
+"""electrical_drive URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("", include("electrical_drive.motors.urls")),
+    path("", include("electrical_drive.accounts.urls")),
+    path("", include("electrical_drive.home.urls")),
+    path("", include("electrical_drive.contact.urls")),
+    path("", include("electrical_drive.cars.urls")),
+    path("", include("electrical_drive.news.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
