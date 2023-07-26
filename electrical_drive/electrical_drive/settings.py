84616767d0cbbe4d8c499615eb449f4f3e359cbc
niki9011 <niki901115@gmail.com>
2023-07-26 03:54:48 +0300

    signal

--- electrical_drive/electrical_drive/settings.py
+++ electrical_drive/electrical_drive/settings.py
@@ -147,7 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pavlov85pavel@gmail.com'
+EMAIL_HOST_USER = 'niki901115@gmail.com'
 EMAIL_HOST_PASSWORD = 'gxlizletaiogppny'
 
 
