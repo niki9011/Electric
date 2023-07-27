d5e8285f5ef4b7d644b3676d0e4721ce085ed361
niki9011 <niki901115@gmail.com>
2023-07-27 22:12:59 +0300

    update RegisterUserForm

--- electrical_drive/electrical_drive/accounts/forms.py
+++ electrical_drive/electrical_drive/accounts/forms.py
@@ -16,44 +16,9 @@ class ProfileCreateForm(forms.ModelForm):
 UserModel = get_user_model()
 
 
-class RegisterUserForm(auth_forms.UserCreationForm):
-    consent = forms.BooleanField()
-    first_name = forms.CharField(
-        max_length=30,
-        required=True
-    )
-
-    password2 = forms.CharField(
-        label=_("Repeat Password"),
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
-        strip=False,
-        help_text=_("Repeat password, please"),
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['password1'].help_text = _('It works')
-
-    def save(self, commit=True):
-        user = super().save(commit)
-
-        profile = Profile(
-            first_name=self.cleaned_data['first_name'],
-            user=user,
-        )
-        if commit:
-            profile.save()
-
-        return user
-
-    class Meta(auth_forms.UserCreationForm.Meta):
-        model = UserModel
-        fields = ('email',)
-
-
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField()
-    # picture = forms.URLField()
+
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
@@ -77,4 +42,3 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ["user"]
-
