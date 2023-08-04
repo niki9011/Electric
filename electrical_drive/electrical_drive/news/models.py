115cb5c7beb363499154a1b0d75ec0859c8ed726
niki9011 <niki901115@gmail.com>
2023-08-04 16:28:00 +0300

    news update

--- electrical_drive/electrical_drive/news/models.py
+++ electrical_drive/electrical_drive/news/models.py
@@ -3,13 +3,11 @@ import datetime
 from django.db import models
 
 
-
 class Allnews(models.Model):
     subject = models.CharField(max_length=300, null=False, blank=False)
     image = models.ImageField(upload_to="news_images",)
     description = models.TextField()
     data = models.DateTimeField(default=datetime.datetime.now)
-
 
     def __str__(self):
         return f"{self.id}/ {self.subject}/ {self.data}"
