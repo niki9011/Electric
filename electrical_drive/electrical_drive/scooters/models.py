fcfc82f9383ce4b6002fa88dbf9a3a7b0032f88d
niki9011 <niki901115@gmail.com>
2023-08-13 00:16:40 +0300

    update models and fixed button add

--- electrical_drive/electrical_drive/scooters/models.py
+++ electrical_drive/electrical_drive/scooters/models.py
@@ -12,9 +12,10 @@ class Scooters(models.Model):
     category = models.CharField(max_length=15, null=False, blank=False, choices=CHOOSES)
     brand = models.CharField(max_length=25)
     model = models.CharField(max_length=25)
+    power = models.PositiveIntegerField()
     image = models.ImageField(upload_to="motorbikes_images",)
     description = models.TextField()
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
 
     def __str__(self):
         return f"{self.id}/ {self.model}/ {self.brand}"
